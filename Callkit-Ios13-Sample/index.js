3d22010b9fb3cf612ed604a8cab3fc10f2c7b6f2
okumurarin96 <nguyenquangkyanh@gmail.com>
2020-06-26 13:59:43 +0700

    update Notification for android

--- Callkit-Ios13-Sample/index.js
+++ Callkit-Ios13-Sample/index.js
@@ -5,31 +5,40 @@
 import {AppRegistry, Platform} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
-import CallScreen from './src/CallScreen';
 import messaging from '@react-native-firebase/messaging';
 import notifee from '@notifee/react-native';
 
 if (Platform.OS === 'android') {
   async function onMessageReceived(message) {
-    console.log('message' + JSON.stringify(message));
+    const data = JSON.parse(message.data.data);
+    const callStatus = data.callStatus;
+    const from = data.from.number;
+    const notificationId = '11111'; // YOUR_NOTIFICATION_ID
+    console.log('data: ' + callStatus);
     const channelId = await notifee.createChannel({
       id: 'YOUR_CHANNEL_ID',
       name: 'ChannelName',
       vibration: true,
     });
-
-    await notifee.displayNotification({
-      id: '123456789',
-      title: 'call 2',
-      body: 'New incoming Call',
-      android: {
-        channelId,
-        pressAction: {
-          id: 'default',
-          mainComponent: appName,
-        },
-      },
-    });
+    switch (callStatus) {
+      case 'started':
+        await notifee.displayNotification({
+          id: notificationId,
+          title: 'Incoming Call',
+          body: 'Call from ' + from,
+          android: {
+            channelId,
+            pressAction: {
+              id: 'default',
+              mainComponent: appName,
+            },
+          },
+        });
+        break;
+      case 'ended':
+        await notifee.cancelNotification(notificationId);
+        break;
+    }
   }
 
   messaging().setBackgroundMessageHandler(onMessageReceived);
