847e942c1257a61fbe40e90dcd1aa334ccc17a7e
okumurarin96 <nguyenquangkyanh@gmail.com>
2020-07-23 16:52:12 +0700

    add file google-services.json

--- Callkit-Ios13-Sample/src/CallScreen.js
+++ Callkit-Ios13-Sample/src/CallScreen.js
@@ -121,7 +121,6 @@ class CallScreen extends Component {
 
   renderSpeakerImage = () => {
     var imgSource = this.props.isSpeaker ? speakerImg_selected : speakerImg;
-    console.log("this.props.isSpeaker: " + this.props.isSpeaker);
     return <Image style={styles.button} source={imgSource} />;
   };
 
